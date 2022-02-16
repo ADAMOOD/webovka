89c95df74808946bcd9e9c69424f2a07bebea0df
ADAMOOD <99761968+ADAMOOD@users.noreply.github.com>
2022-02-16 19:48:43 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+function funkce_burger(x) {
+    x.classList.toggle("change");
+
+    const hamburger=document.getElementsByClassName('burger');
+    const nav_odkazy=document.getElementById('odkazy');
+    const odkaz=document.getElementsByClassName('btn');
+    let i;
+    {
+      nav_odkazy.classList.toggle('show');
+      for(i=0;i<6;i++)
+      {
+        odkaz[i].classList.toggle('show');
+      }
+     
+    
+    }
+  }
+
